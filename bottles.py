e2ff33295f9c00ee64353d9eb3c0173a7ebfe757
Sophia Salus <ssalus22@students.mchenry.edu>
2025-02-17 14:46:17 -0600

    Commit

---
+++ bottles.py
@@ -0,0 +1,13 @@
+'''# create a variable and set to 99 
+# bottles = 99
+# while loop > 1
+# print the lines using the variable
+# if statement not plural (1 bottle of beer)
+# if statement 0 - (no bottles)
+'''
+
+
+bottles = 99
+while bottles > 1:
+    print(f"{bottles} bottles of beer on the wall")
+    bottles -= 1
